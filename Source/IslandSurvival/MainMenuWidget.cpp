b52bf191916a5f026e0910b67c21be2d2f127fae
DJHPersonal <69775476+DJHStudent@users.noreply.github.com>
2021-11-10 04:30:53 +1100

    Update MainMenuWidget.cpp

--- Source/IslandSurvival/MainMenuWidget.cpp
+++ Source/IslandSurvival/MainMenuWidget.cpp
@@ -20,71 +20,60 @@ bool UMainMenuWidget::Initialize() //run when the widget gets created
 
 	MainGameInstance = Cast<UMainGameInstance>(UGameplayStatics::GetGameInstance(GetWorld())); //get a reference to the Game Instance using on each client
 	Loading->SetVisibility(ESlateVisibility::Hidden);
-	//ErrorMenu->SetVisibility(ESlateVisibility::Hidden);
 
 	return true;
 }
 
-void UMainMenuWidget::OnHostButtonPressed()
+void UMainMenuWidget::OnHostButtonPressed() //called when host button pressed
 {
-	UE_LOG(LogTemp, Warning, TEXT("Host Button Pressed"));
-	//GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Green, "Host Button Pressed");
 	if (MainGameInstance)
-		MainGameInstance->HostSession();
+		MainGameInstance->HostSession(); //start a new session
 }
 
-void UMainMenuWidget::OnJoinButtonPressed()
+void UMainMenuWidget::OnJoinButtonPressed() //when join button pressed
 {
-	UE_LOG(LogTemp, Warning, TEXT("Join Button Pressed"));
-	//GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, "Join Button Pressed");
-	UpdateJoiningText("Finding Nearest Session");
+	UpdateJoiningText("Finding Nearest Session"); //tell user that code looking for session
 	if (MainGameInstance)
 	{
-		Loading->SetVisibility(ESlateVisibility::Visible);
-		MainGameInstance->JoinSession();
+		Loading->SetVisibility(ESlateVisibility::Visible); //show message that currently looking for a session
+		MainGameInstance->JoinSession(); //go about searching for and joining the session
 	}
 }
 
-void UMainMenuWidget::OnQuitButtonPressed()
+void UMainMenuWidget::OnQuitButtonPressed() //when quit button pressed
 {
-	UE_LOG(LogTemp, Warning, TEXT("Quit Button Pressed"));
-	//GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Red, "Quit Button Pressed");
-	GetWorld()->GetFirstPlayerController()->ConsoleCommand("quit");
+	GetWorld()->GetFirstPlayerController()->ConsoleCommand("quit"); //close the game
 }
 
-void UMainMenuWidget::OnCancelButtonPressed()
+void UMainMenuWidget::OnCancelButtonPressed() //when searching for a session and cancel button pressed
 {
-	UE_LOG(LogTemp, Warning, TEXT("Cancel Button Pressed"));
-	ButtonCancel->SetVisibility(ESlateVisibility::Hidden);
-	//GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Red, "Cancel Button Pressed");
-	UpdateJoiningText("Cancelling Session Joining");
+	ButtonCancel->SetVisibility(ESlateVisibility::Hidden); //disable buttin so cannot be pressed again
+	UpdateJoiningText("Cancelling Session Joining"); //notify user cancelling search
 	if (MainGameInstance)
 	{
-		MainGameInstance->CancelFindSession();
+		MainGameInstance->CancelFindSession(); //actually cancel the search
 		FTimerHandle CancellingTimer;
-		GetWorld()->GetTimerManager().SetTimer(CancellingTimer, this, &UMainMenuWidget::HideLoadingMenu, 2.0f, false);
+		GetWorld()->GetTimerManager().SetTimer(CancellingTimer, this, &UMainMenuWidget::HideLoadingMenu, 2.0f, false); //in 2 seconds hide cancel message
 	}
 }
 
 void UMainMenuWidget::UpdateJoiningText(FString Text)
 {
 	TextBlockJoining->SetText(FText::FromString(Text));
-
 }
 
-void UMainMenuWidget::OnOKButtonPressed()
+void UMainMenuWidget::OnOKButtonPressed() //called when Ok button clicked after onnection crashed
 {
-	ErrorMenu->SetVisibility(ESlateVisibility::Hidden);
-	MainGameInstance->bCrashed = false;
+	ErrorMenu->SetVisibility(ESlateVisibility::Hidden); //hide this error menu
+	MainGameInstance->bCrashed = false; //update Player to no longer be crashed
 }
 
-void UMainMenuWidget::ShowErrorMenu()
+void UMainMenuWidget::ShowErrorMenu() //update UI to show error message if connection failed
 {
 	ErrorMenu->SetVisibility(ESlateVisibility::Visible);
-	//GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Red, "Show Error Menu");
 }
 
-void UMainMenuWidget::HideLoadingMenu()
+void UMainMenuWidget::HideLoadingMenu() //if searching for session and cancelled update UI
 {
 	Loading->SetVisibility(ESlateVisibility::Hidden);
 	ButtonCancel->SetVisibility(ESlateVisibility::Visible);
